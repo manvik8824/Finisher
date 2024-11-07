390f01231330a0d37fa772cad5bc071cf81779c2
manvik8824 <finishermods1@gmail.com>
2024-11-07 08:04:21 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7385390783:AAGSQhSdKb3LfgjO7q38eQO7IwwFFPHekJI")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("ILLEGALCHEAT78")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("844639708")
+
+
+
+
+
+
+
+  
